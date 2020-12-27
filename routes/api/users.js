0e0db827c63684939d5f3179b8e729f921435671
alanashannon <alashannon@gmail.com>
2020-12-26 18:00:54 -0800

    Add login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -29,9 +29,29 @@ router.post('/register', (req, res) => {
                 })
             })
 
-            // newUser.save().then(user => res.send(user)).catch(err => res.send(err));
         }
     })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email; 
+    const password = req.body.password; 
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: "This user does not exist." })
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: "Success!"})
+                    } else {
+                        return res.status(400).json({ password: "Incorrect password." })
+                    }
+                })
+        })
 })
 
 module.exports = router; 
