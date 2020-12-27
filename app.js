c3cf44ec09cc2f60f01f3edec7f51b7a6037fcc4
alanashannon <alashannon@gmail.com>
2020-12-26 18:46:52 -0800

    Add passport.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const users = require("./routes/api/users")
 const tweets = require("./routes/api/tweets")
 const bodyParser = require("body-parser");
 const User = require('./models/User'); 
+const passport = require('passport'); 
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -19,19 +20,14 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-    // const user = new User({
-    //     handle: "jim", 
-    //     email: "jim@jim.jim", 
-    //     password: "password"
-    // })
-    // user.save()
     res.send("Hello World!!")
 });
 
+app.use(passport.initialize()); 
+require('./config/passport')(passport); 
+
 app.use("/api/users", users)
 app.use("/api/tweets", tweets)
-
-
 
 const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Server is running on port ${port}`)); 
