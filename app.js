2aa5720d73937c856b1a701a1132c0076a82d50f
alanashannon <alashannon@gmail.com>
2020-12-26 17:45:35 -0800

    Add register users route

--- app.js
+++ app.js
@@ -7,25 +7,31 @@ const tweets = require("./routes/api/tweets")
 const bodyParser = require("body-parser");
 const User = require('./models/User'); 
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to mongoDB"))
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-    const user = new User({
-        handle: "jim", 
-        email: "jim@jim.jim", 
-        password: "password"
-    })
-    user.save()
+    // const user = new User({
+    //     handle: "jim", 
+    //     email: "jim@jim.jim", 
+    //     password: "password"
+    // })
+    // user.save()
     res.send("Hello World!!")
 });
 
 app.use("/api/users", users)
 app.use("/api/tweets", tweets)
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
+
 
 const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Server is running on port ${port}`)); 
