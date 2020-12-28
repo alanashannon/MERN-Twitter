a5f3ff9bac198d2bbfc026c4b2b80cbf5c8d4a67
alanashannon <alashannon@gmail.com>
2020-12-27 20:26:52 -0800

    Add keys

--- frontend/src/components/session/login_form.js
+++ frontend/src/components/session/login_form.js
@@ -44,9 +44,9 @@ class LoginForm extends React.Component {
         return(
             <ul>
                 {Object.keys(this.state.errors).map((error, i) => (
-                    <li key={`error-${li}`}>
+                    <li>
                         {this.state.errors[error]}
-                    </li>
+                    </li> //key = {`error-${li}`}
                 ))}
             </ul>
         )
